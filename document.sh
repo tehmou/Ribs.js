471be55de25cd6e6e9f03d7184342065184dff9e
Timo Tuominen <timo.tuominen@gmail.com>
2011-05-22 20:43:39 +0200

    Fixed build scripts a bit.

--- document.sh
+++ document.sh
@@ -2,8 +2,8 @@ SRC_DIR=src
 TARGET_DIR=jsdocs
 
 echo "Clearing the target directory ${TARGET_DIR}..."
-rm -rf TARGET_DIR
-mkdir TARGET_DIR
+rm -rf $TARGET_DIR
+mkdir $TARGET_DIR
 
 echo "Building the jsdocs from ${SRC_DIR}..."
 cd lib/jsdoc-toolkit
---
+++ make.sh
@@ -0,0 +1,2 @@
+./document.sh
+./deploy.sh
