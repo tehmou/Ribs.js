f29526efe67f06559f54508e9a1b71c0e95ecf99
Timo Tuominen <timo.tuominen@gmail.com>
2011-05-26 00:03:46 +0200

    Docs for branch 0.0.831

---
+++ document.sh
@@ -0,0 +1,11 @@
+SRC_DIR=src
+TEMPLATE_DIR=jsdocs/templates
+TARGET_DIR=jsdocs/generated
+
+echo "Clearing the target directory ${TARGET_DIR}..."
+rm -rf $TARGET_DIR
+mkdir $TARGET_DIR
+
+echo "Building the jsdocs from ${SRC_DIR}..."
+cd lib/jsdoc-toolkit
+./jsrun.sh ../../$SRC_DIR -d=../../$TARGET_DIR -t=../../$TEMPLATE_DIR -r=3
