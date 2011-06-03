03cb8129efb2be1da70b0ea50b22e0d05b85fedf
Timo Tuominen <timo.tuominen@gmail.com>
2011-06-03 13:21:54 +0200

    Another parsing test added with some test helpers for Jasmine.

---
+++ test/helpers/mixintesthelpers.js
@@ -0,0 +1,21 @@
+jasmine.Matchers.prototype.toHaveNumberOfMixinClasses = function (expected) {
+    if (this.actual.mixinClasses.length !== expected) {
+        throw "Wrong number of mixin classes found (" + this.actual.mixinClasses.length + "/" + expected + ")";
+    }
+};
+
+jasmine.Matchers.prototype.toContainOneMixinWithElementSelector = function (expected) {
+    var found = false;
+    _.each(this.actual.mixinClasses, function (mixin) {
+        if (mixin.elementSelector === expected) {
+            if (found) {
+                throw "Found more than one mixins with selector '" + expected + "'";
+            } else {
+                found = true;
+            }
+        }
+    });
+    if (!found) {
+        throw "Could not find a mixin with the selector '" + expected + "'";
+    }
+};
--- test/spec/spec_parser.js
+++ test/spec/spec_parser.js
@@ -5,17 +5,28 @@ describe("Ribs.mixinBase.selfParsing", function () {
         mixin = _.clone(Ribs.mixinBase.selfParsing);
         parser = Ribs.createMixinDefinitionParser({
             mixinLibrary: {
+                plain: {},
                 composite: Ribs.mixinBase.composite
             }
         });
     });
 
     it("Should consider elements of an array as mixins", function () {
-        
-    });
+        var def = [
+                { plain: { elementSelector: ".mySel" }},
+                { plain: { param1: "testparam" }},
+                { plain: {}}
+            ],
+            composite = _.clone(Ribs.mixinBase.composite);
 
-    it("Should place the parsed mixinClasses into the given parameter mixin", function () {
+        parser.createCompositeFromDefinitions({
+            mixinDefinitions: def,
+            composite: composite
+        });
 
+        expect(composite.mixinClasses[0].elementSelector).toEqual(".mySel");
+        expect(composite.mixinClasses[1].param1).toEqual("testparam");
+        expect(composite).toHaveNumberOfMixinClasses(def.length);
     });
 
     it("Should be able to read elementSelector from the high level object keys", function () {
@@ -25,24 +36,12 @@ describe("Ribs.mixinBase.selfParsing", function () {
                 "myEl": []
             },
             composite = parser.createCompositeFromDefinitions({ mixinDefinitions: def });
-        var numExpectedMixins = 0;
-        _.each(def, function (defValue, defKey) {
-            var found = false;
-            _.each(composite.mixinClasses, function (realValue) {
-                if (realValue.elementSelector === defKey) {
-                    found = true;
-                }
-            });
-            if (!found) {
-                throw "Could not find a mixin with the selector '" + defKey + "'";
-            }
-            numExpectedMixins++;
-        });
 
-        var numCreatedMixins = composite.mixinClasses.length;
-        if (numCreatedMixins !== numExpectedMixins) {
-            throw "Found wrong number of mixins in the composite (" + numCreatedMixins + "/" + numExpectedMixins + ")";
-        }
+
+        expect(composite).toContainOneMixinWithElementSelector("");
+        expect(composite).toContainOneMixinWithElementSelector("elementOne");
+        expect(composite).toContainOneMixinWithElementSelector("myEl");
+        expect(composite).toHaveNumberOfMixinClasses(3);
     });
 
     it("Should consider the key in a mixin definition as the base, and the value as the overrides", function () {
